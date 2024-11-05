189b74a1f8f155f977108ca39aa9df1d9da17a36
Tim <81175852+wharvex@users.noreply.github.com>
2024-11-04 20:30:56 -0500

    additional files and prompts

--- NewApply.ps1
+++ NewApply.ps1
@@ -2,6 +2,8 @@ function New-Apply {
     [CmdletBinding(SupportsShouldProcess)]
     $pos = Read-Host "Position"
     $emp = Read-Host "Employer"
+    $loc = Read-Host "Location"
+    $sal = Read-Host "Salary"
 
     $count_path = "$($env:userprofile)\ApplyCount.txt"
     if (![System.IO.File]::Exists($count_path)) {
@@ -19,10 +21,16 @@ function New-Apply {
     Set-Content -Path $count_path -Value $ac
     $apply_folder_name = "Apply$($ac)"
     mkdir $apply_folder_name
+    Copy-Item -Path .\resumes\resume_main.pptx -Destination $apply_folder_name
     $pos | Out-File -FilePath "$($apply_folder_name)/position.txt"
     $emp | Out-File -FilePath "$($apply_folder_name)/employer.txt"
+    $loc | Out-File -FilePath "$($apply_folder_name)/location.txt"
+    $sal | Out-File -FilePath "$($apply_folder_name)/salary.txt"
     "cover letter" | Out-File -FilePath "$($apply_folder_name)/cover_letter.txt"
     "john smith`nhr`naddr`naddr" | Out-File -FilePath "$($apply_folder_name)/cover_letter_addr.txt"
-    "duties" | Out-File -FilePath "$($apply_folder_name)/duties.txt"
+    "description" | Out-File -FilePath "$($apply_folder_name)/description.txt"
+    "required skills" | Out-File -FilePath "$($apply_folder_name)/required-skills.txt"
+    "preferred skills" | Out-File -FilePath "$($apply_folder_name)/preferred-skills.txt"
+    "applied on" | Out-File -FilePath "$($apply_folder_name)/applied-on.txt"
     vim $apply_folder_name
 }
