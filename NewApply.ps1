5d3f05bd0e1ea336284efbb66c9b5d32702f7adf
Tim <81175852+wharvex@users.noreply.github.com>
2024-11-07 10:13:01 -0500

    add more doc-creation commands

--- NewApply.ps1
+++ NewApply.ps1
@@ -4,6 +4,7 @@ function New-Apply {
     $emp = Read-Host "Employer"
     $loc = Read-Host "Location"
     $sal = Read-Host "Salary"
+    $dis = Read-Host "Discovered Through"
 
     $count_path = "$($env:userprofile)\ApplyCount.txt"
     if (![System.IO.File]::Exists($count_path)) {
@@ -17,20 +18,26 @@ function New-Apply {
         return
     }
 
+    # Update ApplyCount
     $ac = [int]$lines + 1
     Set-Content -Path $count_path -Value $ac
     $apply_folder_name = "Apply$($ac)"
+
+    # Create the apply folder
     mkdir $apply_folder_name
-    Copy-Item -Path .\resumes\resume_main.pptx -Destination $apply_folder_name
+
+    Copy-Item -Path .\resumes\resume_main.pptx -Destination "$($apply_folder_name)/resume.pptx"
+    Copy-Item -Path .\CoverLetters\NewTemplate.txt -Destination "$($apply_folder_name)/cover_letter.txt"
+    Copy-Item -Path .\CoverLetters\NewTemplate.docx -Destination "$($apply_folder_name)/cover_letter.docx"
     $pos | Out-File -FilePath "$($apply_folder_name)/position.txt"
     $emp | Out-File -FilePath "$($apply_folder_name)/employer.txt"
     $loc | Out-File -FilePath "$($apply_folder_name)/location.txt"
     $sal | Out-File -FilePath "$($apply_folder_name)/salary.txt"
-    "cover letter" | Out-File -FilePath "$($apply_folder_name)/cover_letter.txt"
+    $dis | Out-File -FilePath "$($apply_folder_name)/disc_thru.txt"
     "john smith`nhr`naddr`naddr" | Out-File -FilePath "$($apply_folder_name)/cover_letter_addr.txt"
     "description" | Out-File -FilePath "$($apply_folder_name)/description.txt"
-    "required skills" | Out-File -FilePath "$($apply_folder_name)/required-skills.txt"
-    "preferred skills" | Out-File -FilePath "$($apply_folder_name)/preferred-skills.txt"
-    "applied on" | Out-File -FilePath "$($apply_folder_name)/applied-on.txt"
+    "required skills" | Out-File -FilePath "$($apply_folder_name)/required_skills.txt"
+    "preferred skills" | Out-File -FilePath "$($apply_folder_name)/preferred_skills.txt"
+    "applied on" | Out-File -FilePath "$($apply_folder_name)/applied_on.txt"
     vim $apply_folder_name
 }
